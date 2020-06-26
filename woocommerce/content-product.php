04ee694faa723a95e9d7f4866c2b42b0009fcdd0
Fer221075 <67390145+Fer221075@users.noreply.github.com>
2020-06-26 10:55:25 -0500

    change % pv

--- woocommerce/content-product.php
+++ woocommerce/content-product.php
@@ -26,9 +26,6 @@ if (empty($product) || !$product->is_visible()) {
                 $por = '10%';
             }
             
-            if (get_the_ID() == 41) {
-                $por = '10%';
-            }
            
         }
 
