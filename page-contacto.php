3c27afc3628e50f82ead0553b56ea0471447360b
Fer221075 <67390145+Fer221075@users.noreply.github.com>
2020-11-13 10:52:53 -0500

    Update page-contacto.php

--- page-contacto.php
+++ page-contacto.php
@@ -64,7 +64,7 @@ while ( have_posts() ) : the_post(); ?>
 </g>
 </svg>
 
-                <b> Dirección: </b>Carrera 68h #
+                <b> Dirección: </b>Carrera 68h # 73A - 29
                 
               </li>
               <li>
